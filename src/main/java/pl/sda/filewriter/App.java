a156d44cced1f3427a27f549604ca00ddaac466a
Joanna Kęprowska <a.keprowska@gmail.com>
2019-08-03 13:36:56 +0200

    Dodanie wątków 3 i 4

--- src/main/java/pl/sda/filewriter/App.java
+++ src/main/java/pl/sda/filewriter/App.java
@@ -35,16 +35,53 @@ public class App {
             }
         }
 
-        public static void main(String[] args) {
-            Task1 task1 = new Task1();
-            Task2 task2 = new Task2();
-            //uruchomienie watkow:
-            Thread thread1 = new Thread(task1);
-            Thread thread2 = new Thread(task2);
+        static class Task3 implements Runnable {
 
-            thread1.start();
-            thread2.start();
+            @Override
+            public void run() {
+                for (int k = 0; k < 1000; k++) {
+                    System.out.println("WATEK 3: " + k);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
+            static class Task4 implements Runnable {
+
+                @Override
+                public void run() {
+                    for (int l = 0; l < 1000; l++) {
+                        System.out.println("WATEK 3: " + l);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+
+                public static void main(String[] args) {
+                    Task1 task1 = new Task1();
+                    Task2 task2 = new Task2();
+                    Task3 task3 = new Task3();
+                    Task4 task4 = new Task4();
+                    //uruchomienie watkow:
+                    Thread thread1 = new Thread(task1);
+                    Thread thread2 = new Thread(task2);
+                    Thread thread3 = new Thread(task3);
+                    Thread thread4 = new Thread(task4);
+
+                    thread1.start();
+                    thread2.start();
+                    thread3.start();
+                    thread4.start();
+
+                }
+            }
         }
     }
 }
