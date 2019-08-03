8fbd1c979597d4dc91d023a2a648dbe7235e2288
Joanna Kęprowska <a.keprowska@gmail.com>
2019-08-03 14:27:07 +0200

    Stworzenie pola instance, który jest null

--- src/main/java/pl/sda/filewriter/FileWriterSingleton.java
+++ src/main/java/pl/sda/filewriter/FileWriterSingleton.java
@@ -1,9 +1,19 @@
 package pl.sda.filewriter;
 
+import java.io.File;
+
 public class FileWriterSingleton {
     //zabezpieczenie klasy przed stworzeniem obiektu tej klasy
     //a wiec prywatny konstruktor
-    private FileWriterSingleton(){
+    private static FileWriterSingleton instance;
+     //pole instance jest w tym momencie nullem
+    private FileWriterSingleton() {
+
+    }
+    public static FileWriterSingleton getInstance(){
+        if (instance == null){
+            instance = new FileWriterSingleton();
+        }
 
     }
 }
