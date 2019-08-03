8cf875adcc5e3e4a051e14bfea549130152c0366
Joanna Kęprowska <a.keprowska@gmail.com>
2019-08-03 13:30:48 +0200

    Teraz nasze watki dzialaja wolniej przez petle sleep. Przeplatają się znacznie wyraźniej

--- src/main/java/pl/sda/filewriter/App.java
+++ src/main/java/pl/sda/filewriter/App.java
@@ -11,6 +11,11 @@ public class App {
 
             for (int i = 0; i < 1000; i++) {
                 System.out.println("WATEK 1: " + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -21,20 +26,25 @@ public class App {
         public void run() {
             for (int j = 0; j < 1000; j++) {
                 System.out.println("WATEK 2: " + j);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
 
+                }
             }
         }
-    }
 
-    public static void main(String[] args) {
-        Task1 task1 = new Task1();
-        Task2 task2 = new Task2();
-        //uruchomienie watkow:
-        Thread thread1 = new Thread(task1);
-        Thread thread2 = new Thread(task2);
+        public static void main(String[] args) {
+            Task1 task1 = new Task1();
+            Task2 task2 = new Task2();
+            //uruchomienie watkow:
+            Thread thread1 = new Thread(task1);
+            Thread thread2 = new Thread(task2);
 
-        thread1.start();
-        thread2.start();
+            thread1.start();
+            thread2.start();
 
+        }
     }
 }
