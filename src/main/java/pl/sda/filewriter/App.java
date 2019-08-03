2820fe609f6b2394ee2c0df9227aad24b0d6c7ab
Joanna Kęprowska <a.keprowska@gmail.com>
2019-08-03 13:27:35 +0200

    uruchomienie wątków, ale nie ma określonego który z nich pierwszy się uruchomi + one się przeplatają

--- src/main/java/pl/sda/filewriter/App.java
+++ src/main/java/pl/sda/filewriter/App.java
@@ -2,12 +2,39 @@ package pl.sda.filewriter;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    static class Task1 implements Runnable {
+
+        @Override
+        public void run() {
+
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("WATEK 1: " + i);
+            }
+        }
+    }
+
+    static class Task2 implements Runnable {
+
+        @Override
+        public void run() {
+            for (int j = 0; j < 1000; j++) {
+                System.out.println("WATEK 2: " + j);
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        Task2 task2 = new Task2();
+        //uruchomienie watkow:
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
+
+        thread1.start();
+        thread2.start();
+
     }
 }
