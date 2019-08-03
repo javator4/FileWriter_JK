b0b859d979f52fa5d027864d163188274fa40a2e
Joanna Kęprowska <a.keprowska@gmail.com>
2019-08-03 14:33:16 +0200

    Revert "To jest cała konstrukcja Singletona na pliku"

--- src/main/java/pl/sda/filewriter/FileWriterSingleton.java
+++ src/main/java/pl/sda/filewriter/FileWriterSingleton.java
@@ -3,17 +3,15 @@ package pl.sda.filewriter;
 import java.io.File;
 
 public class FileWriterSingleton {
-
     private static FileWriterSingleton instance;
-
     private FileWriterSingleton() {
 
     }
     public static FileWriterSingleton getInstance(){
         if (instance == null){
             instance = new FileWriterSingleton();
+            return instance;
         }
-        return instance;
 
     }
 }
